bcac6023048a92886b45863a51281a2ffbe1b821
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-20 17:12:38 +0800

    feat(02 doodle jump): platform

--- src/02_Doodle_Jump/main.cpp
+++ src/02_Doodle_Jump/main.cpp
@@ -3,10 +3,31 @@
 
 using namespace sf;
 
+struct point {
+    int x;
+    int y;
+};
+
 int main() {
-    const int w = 320;
-    const int h = 480;
+    srand(time(0));
+
+    const int w = 400;
+    const int h = 533;
     RenderWindow window(VideoMode(w, h), "GameDevLog");
+
+    Texture t1,t2,t3;
+    t1.loadFromFile("images/background.png");
+    t2.loadFromFile("images/platform.png");
+    t3.loadFromFile("images/doodle.png");
+
+    Sprite sBackground(t1), sPlat(t2), sPers(t3);
+
+    point plat[20];
+
+    for (int i = 0; i < 10 ; i++) {
+       plat[i].x = rand() % w;
+       plat[i].y = rand() % h;
+    }
 
     while (window.isOpen()) {
         Event e;
@@ -15,6 +36,14 @@ int main() {
             if (e.type == Event::Closed) {
                 window.close();
             }
+        }
+
+        window.draw(sBackground);
+        window.draw(sPers);
+
+        for (int i = 0; i < 10; i++) {
+            sPlat.setPosition(plat[i].x, plat[i].y);
+            window.draw(sPlat);
         }
 
         window.display();
