d772f317ed22ee8387751abc76f28e63423956d7
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-20 14:54:09 +0800

    feat(01 tetris): t

--- src/01_Tetris/main.cpp
+++ src/01_Tetris/main.cpp
@@ -7,19 +7,19 @@ const int M = 20;
 const int N = 10;
 
 struct Point {
-	int x, y;
-} a[4], b[4];
+    int x, y;
+} origin[4], tmp[4];
 
 int field[M][N] = {{ 0 }};
 
 int figures[7][4] = {
-		{ 1, 3, 5, 7 }, // I
-		{ 2, 4, 5, 7 }, // Z
-		{ 3, 5, 4, 6 }, // S
-		{ 3, 5, 4, 7 }, // T
-		{ 2, 3, 5, 7 }, // L
-		{ 3, 5, 7, 6 }, // J
-		{ 2, 3, 4, 5 }, // O
+        { 1, 3, 5, 7 }, // I
+        { 2, 4, 5, 7 }, // Z
+        { 3, 5, 4, 6 }, // S
+        { 3, 5, 4, 7 }, // T
+        { 2, 3, 5, 7 }, // L
+        { 3, 5, 7, 6 }, // J
+        { 2, 3, 4, 5 }, // O
 };
 
 int main() {
@@ -33,6 +33,15 @@ int main() {
     Sprite s(t);
     s.setTextureRect(IntRect(0, 0, 18, 18));
 
+    int dx = 0;
+    bool rotate = 0;
+    int colorNum = 1;
+
+    float timer = 0f;
+    float delay = 0.3f;
+
+    Clock clock;
+
     while (window.isOpen()) {
         Event e;
 
@@ -41,9 +50,52 @@ int main() {
                 window.close();
             }
         }
+        
+        if (e.type == Event::KeyPressed){
+            if (e.key.code == Keyboard::Up) {
+                rotate = true;
+            } else if (e.key.code == Keyboard::Left) {
+                dx = -1;
+            } else if (e.key.code == Keyboard::Right) {
+                dx = 1;
+            }
+        }
+
+        // move
+        for (int i = 0; i < 4; i++) {
+            tmp[i] = origin[i];
+            origin[i].x += dx;
+        }
+        
+        // rotate
+        if (rotate) {
+            Point p = origin[1]; // center of rotation
+            for (int i = 0; i < 4; i++) {
+                int x = origin[i].y - p.y;
+                int y = origin[i].x - p.x;
+                origin[i].x = p.x - x;
+                origin[i].y = p.y + y;
+            }
+        }
+
+        int n = 3;
+        if (origin[0].x == 0) {
+            for (int i = 0; i < 4; i++) {
+                origin[i].x = figures[n][i] % 2;
+                origin[i].y = figures[n][i] / 2;
+            }
+        }
+
+        dx = 0;
+        rotate = false;
 
         window.clear(Color::White);
-        window.draw(s);
+
+        for (int i = 0; i < 4; i++) {
+            s.setPosition(origin[i].x * 18, origin[i].y * 18);
+            window.draw(s);
+        }
+
         window.display();
     }
 
