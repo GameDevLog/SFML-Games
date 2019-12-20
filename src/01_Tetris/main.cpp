c36ab35c419b38b932b4d9d393a7724190b0b4ce
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-20 14:50:54 +0800

    feat(01 tetris): data struct

--- src/01_Tetris/main.cpp
+++ src/01_Tetris/main.cpp
@@ -3,6 +3,25 @@
 
 using namespace sf;
 
+const int M = 20;
+const int N = 10;
+
+struct Point {
+	int x, y;
+} a[4], b[4];
+
+int field[M][N] = {{ 0 }};
+
+int figures[7][4] = {
+		{ 1, 3, 5, 7 }, // I
+		{ 2, 4, 5, 7 }, // Z
+		{ 3, 5, 4, 6 }, // S
+		{ 3, 5, 4, 7 }, // T
+		{ 2, 3, 5, 7 }, // L
+		{ 3, 5, 7, 6 }, // J
+		{ 2, 3, 4, 5 }, // O
+};
+
 int main() {
     const int w = 320;
     const int h = 480;
