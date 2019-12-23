00e8a938e7b6b95816053d65df774b967fad0b5b
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 09:48:31 +0800

    feat(05 minesweeper): mine & count

--- src/05_Minesweeper/main.cpp
+++ src/05_Minesweeper/main.cpp
@@ -20,6 +20,27 @@ int main() {
     for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 10; j++) {
             sgrid[i][j] = 10;
+            if (rand() % 5 == 0) {
+                grid[i][j] = 9;
+            } else {
+                grid[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            int n = 0;
+            if (grid[i][j] == 9) { continue; }
+            if (grid[i + 1][j] == 9) { n++; } // right
+            if (grid[i][j + 1] == 9) { n++; } // bottom
+            if (grid[i - 1][j] == 9) { n++; } // left
+            if (grid[i][j - 1] == 9) { n++; } // top
+            if (grid[i + 1][j + 1] == 9) { n++; }
+            if (grid[i - 1][j - 1] == 9) { n++; }
+            if (grid[i - 1][j + 1] == 9) { n++; }
+            if (grid[i + 1][j - 1] == 9) { n++; }
+            grid[i][j] = n;
         }
     }
 
@@ -35,6 +56,7 @@ int main() {
         window.clear(Color::White);
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
+                sgrid[i][j] = grid[i][j];
                 s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
                 s.setPosition(i * w, j * w);
                 window.draw(s);
