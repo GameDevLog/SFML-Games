5fa0c74dfd3a5cc18696e2914bf673b76afb618f
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 20:01:03 +0800

    feat(16 asteroids): move

--- src/16_Asteroids/main.cpp
+++ src/16_Asteroids/main.cpp
@@ -8,6 +8,8 @@ using namespace sf;
 const int W = 1200;
 const int H = 800;
 
+float DEGTORAD = 0.017453f;
+
 int main() {
     RenderWindow window(VideoMode(W, H), "GameDevLog");
     window.setFramerateLimit(60);
@@ -20,6 +22,10 @@ int main() {
     sPlayer.setTextureRect(IntRect(40, 0, 40, 40));
     sPlayer.setOrigin(20, 20);
 
+    float x = 300, y = 300;
+    float dx = 0, dy = 0, angle = 0;
+    bool thrust;
+
     while (window.isOpen()) {
         Event e;
 
@@ -28,6 +34,39 @@ int main() {
                 window.close();
             }
         }
+
+        if (Keyboard::isKeyPressed(Keyboard::Right)) { angle += 3; }
+        if (Keyboard::isKeyPressed(Keyboard::Left)) { angle -= 3; }
+
+        if (Keyboard::isKeyPressed(Keyboard::Up)) { thrust = true; }
+        else { thrust = false; }
+
+        // spaceship movement
+        if (thrust) {
+            dx += cos(angle * DEGTORAD) * 0.2;
+            dy += sin(angle * DEGTORAD) * 0.2;
+        } else {
+            dx *= 0.99;
+            dy *= 0.99;
+        }
+
+        int maxSpeed = 15;
+        float speed = sqrt(dx * dx + dy * dy);
+        if (speed > maxSpeed) {
+            dx *= maxSpeed / speed;
+            dy *= maxSpeed / speed;
+        }
+
+        x += dx;
+        y += dy;
+
+        if (x > W) { x = 0; }
+        if (x < 0) { x = W; }
+        if (y > H) { y = 0; }
+        if (y < 0) { y = H; }
+
+        sPlayer.setPosition(x, y);
+        sPlayer.setRotation(angle + 90);
 
         // draw
         window.clear();
