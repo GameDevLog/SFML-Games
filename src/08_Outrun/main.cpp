5a087c6659edc8d25b718afea8be611064ddddd2
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 12:43:24 +0800

    feat(08 outrun): draw sprite

--- src/08_Outrun/main.cpp
+++ src/08_Outrun/main.cpp
@@ -26,6 +26,34 @@ struct Line {
         Y = (1 - scale * (y - camY)) * height / 2;
         W = scale * roadW * width / 2;
     }
+
+    void drawSprite(RenderWindow &window) {
+        Sprite s = sprite;
+        int w = s.getTextureRect().width;
+        int h = s.getTextureRect().height;
+
+        float destX = X + scale * spriteX * width / 2;
+        float destY = Y + 4;
+        float destW = w * W / 266;
+        float destH = h * W / 266;
+
+        destX += destW * spriteX; // offsetX
+        destY += destH * (-1);    // offsetY
+
+        float clipH = destY + destH - clip;
+        if (clipH < 0) {
+            clipH = 0;
+        }
+
+        if (clipH >= destH) {
+            return;
+        }
+
+        s.setTextureRect(IntRect(0, 0, w, h - h * clipH / destH));
+        s.setScale(destW / w, destH / h);
+        s.setPosition(destX, destY);
+        window.draw(s);
+    }
 };
 
 void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2) {
@@ -40,16 +68,68 @@ void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, 
 
 int main() {
     RenderWindow window(VideoMode(width, height), "GameDevLog");
+    window.setFramerateLimit(60);
+
+    Texture t[50];
+    Sprite object[50];
+    for (int i = 1; i <= 7; i++) {
+        t[i].loadFromFile("images/" + std::to_string(i) + ".png");
+        t[i].setSmooth(true);
+        object[i].setTexture(t[i]);
+    }
+
+    Texture bg;
+    bg.loadFromFile("images/bg.png");
+    bg.setRepeated(true);
+    Sprite sBackground(bg);
+    sBackground.setTextureRect(IntRect(0, 0, 5000, 411));
+    sBackground.setPosition(-2000, 0);
+
     std::vector<Line> lines;
 
     for (int i = 0; i < 1600; i++) {
         Line line;
         line.z = i * segL;
+
+        if (i > 300 && i < 700) {
+            line.curve = 0.5;
+        }
+        if (i > 1100) {
+            line.curve = -0.7;
+        }
+
+        if (i < 300 && i % 20 == 0) {
+            line.spriteX = -2.5;
+            line.sprite = object[5];
+        }
+        if (i % 17 == 0) {
+            line.spriteX = 2.0;
+            line.sprite = object[6];
+        }
+        if (i > 300 && i % 20 == 0) {
+            line.spriteX = -0.7;
+            line.sprite = object[4];
+        }
+        if (i > 800 && i % 20 == 0) {
+            line.spriteX = -1.2;
+            line.sprite = object[1];
+        }
+        if (i == 400) {
+            line.spriteX = -1.2;
+            line.sprite = object[7];
+        }
+
+        if (i > 750) {
+            line.y = sin(i / 30.0) * 1500;
+        }
+
         lines.push_back(line);
     }
 
     int N = lines.size();
     int pos = 0;
+    float playerX = 0;
+    int H = 1500;
 
     while (window.isOpen()) {
         Event e;
@@ -60,16 +140,50 @@ int main() {
             }
         }
 
-        if (Keyboard::isKeyPressed(Keyboard::Up)) { pos += 200; }
-        if (Keyboard::isKeyPressed(Keyboard::Down)) { pos -= 200; }
+        int speed = 0;
 
-        window.clear();
+        if (Keyboard::isKeyPressed(Keyboard::Right)) { playerX += 0.1; }
+        if (Keyboard::isKeyPressed(Keyboard::Left)) { playerX -= 0.1; }
+        if (Keyboard::isKeyPressed(Keyboard::Up)) { speed = 200; }
+        if (Keyboard::isKeyPressed(Keyboard::Down)) { speed = -200; }
+        if (Keyboard::isKeyPressed(Keyboard::Tab)) { speed *= 3; }
+        if (Keyboard::isKeyPressed(Keyboard::W)) { H += 100; }
+        if (Keyboard::isKeyPressed(Keyboard::S)) { H -= 100; }
+
+        pos += speed;
+        while (pos >= N * segL) {
+            pos -= N * segL;
+        }
+        while (pos < 0) {
+            pos += N * segL;
+        }
+
+        window.clear(Color(105, 205, 4));
+        window.draw(sBackground);
         int startPos = pos / segL;
+        int camH = lines[startPos].y + H;
+        if (speed > 0) {
+            sBackground.move(-lines[startPos].curve * 2, 0);
+        }
+        if (speed < 0) {
+            sBackground.move(lines[startPos].curve * 2, 0);
+        }
+
+        int maxy = height;
+        float x = 0, dx = 0;
 
         // draw road
         for (int n = startPos; n < startPos + 300; n++) {
             Line &l = lines[n % N];
-            l.project(0, 1500, pos);
+            l.project(playerX * roadW - x, camH, startPos * segL - (n >= N ? N * segL : 0));
+            x += dx;
+            dx += l.curve;
+
+            l.clip = maxy;
+            if (l.Y >= maxy) {
+                continue;
+            }
+            maxy = l.Y;
 
             Color grass = (n / 3) % 2 ? Color(16, 200, 16) : Color(0, 154, 0);
             Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(0, 0, 0);
@@ -82,6 +196,11 @@ int main() {
             drawQuad(window, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
         }
         
+        // draw object
+        for (int n = startPos + 300; n > startPos; n--) {
+            lines[n % N].drawSprite(window);
+        }
+
         window.display();
     }
 
