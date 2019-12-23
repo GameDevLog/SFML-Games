ef86d02eaca725a52b00db31d21e50f43c1f451b
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 19:52:37 +0800

    feat(16 asteroids): background & spaceship

--- src/16_Asteroids/main.cpp
+++ src/16_Asteroids/main.cpp
@@ -1,12 +1,24 @@
 #include <SFML/Graphics.hpp>
 #include <ctime>
+#include <list>
+#include <cmath>
 
 using namespace sf;
 
+const int W = 1200;
+const int H = 800;
+
 int main() {
-    const int w = 320;
-    const int h = 480;
-    RenderWindow window(VideoMode(w, h), "GameDevLog");
+    RenderWindow window(VideoMode(W, H), "GameDevLog");
+    window.setFramerateLimit(60);
+
+    Texture t1, t2;
+    t1.loadFromFile("images/spaceship.png");
+    t2.loadFromFile("images/background.jpg");
+
+    Sprite sPlayer(t1), sBackground(t2);
+    sPlayer.setTextureRect(IntRect(40, 0, 40, 40));
+    sPlayer.setOrigin(20, 20);
 
     while (window.isOpen()) {
         Event e;
@@ -17,6 +29,10 @@ int main() {
             }
         }
 
+        // draw
+        window.clear();
+        window.draw(sBackground);
+        window.draw(sPlayer);
         window.display();
     }
 
