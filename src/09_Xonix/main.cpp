3eb7e08f6d3445dc4a6b392830a8b0fdf6281520
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 12:51:26 +0800

    feat(09 xonix): move

--- src/09_Xonix/main.cpp
+++ src/09_Xonix/main.cpp
@@ -3,12 +3,32 @@
 
 using namespace sf;
 
+const int M = 25;
+const int N = 40;
+
+int grid[M][N] = { 0 };
+int ts = 18; // tile size
+
 int main() {
-    const int w = 320;
-    const int h = 480;
-    RenderWindow window(VideoMode(w, h), "GameDevLog");
+    srand(time(0));
+
+    RenderWindow window(VideoMode(N * ts, M * ts), "GameDevLog");
+    window.setFramerateLimit(60);
+
+    Texture t1;
+    t1.loadFromFile("images/tiles.png");
+
+    Sprite sTile(t1);
+
+    int x = 0, y = 0, dx = 0, dy = 0;
+    float timer = 0, delay = 0.07;
+    Clock clock;
 
     while (window.isOpen()) {
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
         Event e;
 
         while (window.pollEvent(e)) {
@@ -16,6 +36,24 @@ int main() {
                 window.close();
             }
         }
+
+        if (Keyboard::isKeyPressed(Keyboard::Left)) { dx = -1;  dy = 0; };
+        if (Keyboard::isKeyPressed(Keyboard::Right)) { dx = 1; dy = 0; };
+        if (Keyboard::isKeyPressed(Keyboard::Up)) { dx = 0; dy = -1; };
+        if (Keyboard::isKeyPressed(Keyboard::Down))  { dx = 0; dy = 1; };
+
+        if (timer > delay) {
+            x += dx;
+            y += dy;
+            timer = 0;
+        }
+
+        // draw
+        window.clear();
+
+        sTile.setTextureRect(IntRect(36, 0, ts, ts));
+        sTile.setPosition(x * ts, y * ts);
+        window.draw(sTile);
 
         window.display();
     }
