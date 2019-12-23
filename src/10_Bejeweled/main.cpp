bff5597a57a3e2d5f9b77cb62b6bb0cabf15dca9
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 14:17:30 +0800

    feat(10 bejeweled): bg & gems

--- src/10_Bejeweled/main.cpp
+++ src/10_Bejeweled/main.cpp
@@ -3,10 +3,39 @@
 
 using namespace sf;
 
+int ts = 54; // tile size
+Vector2i offset(48, 24);
+
+struct piece {
+  int x, y, col, row, kind, match, alpha;
+  piece() {
+    match = 0;
+    alpha = 255;
+  }
+} grid[10][10];
+
 int main() {
-    const int w = 320;
+    srand(time(0));
+    const int w = 740;
     const int h = 480;
     RenderWindow window(VideoMode(w, h), "GameDevLog");
+    window.setFramerateLimit(60);
+
+    Texture t1, t2;
+    t1.loadFromFile("images/background.png");
+    t2.loadFromFile("images/gems.png");
+
+    Sprite background(t1), gems(t2);
+
+    for (int i = 1; i <= 8; i++) {
+        for (int j = 1; j <= 8; j++) {
+            grid[i][j].kind = rand() % 3;
+            grid[i][j].col = j;
+            grid[i][j].row = i;
+            grid[i][j].x = j * ts;
+            grid[i][j].y = i * ts;
+        }
+    }
 
     while (window.isOpen()) {
         Event e;
@@ -14,6 +43,20 @@ int main() {
         while (window.pollEvent(e)) {
             if (e.type == Event::Closed) {
                 window.close();
+            }
+        }
+
+        // draw
+        window.draw(background);
+
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                piece p = grid[i][j];
+                gems.setTextureRect(IntRect(p.kind * 49, 0, 49, 49));
+                gems.setColor(Color(255, 255, 255, p.alpha));
+                gems.setPosition(p.x, p.y);
+                gems.move(offset.x - ts, offset.y - ts);
+                window.draw(gems);
             }
         }
 
