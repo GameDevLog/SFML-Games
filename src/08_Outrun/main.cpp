0b4e02a797536194945ccf62c0d375a7afa18424
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 12:23:25 +0800

    feat(08 outrun): 3d -> 2d

--- src/08_Outrun/main.cpp
+++ src/08_Outrun/main.cpp
@@ -6,6 +6,27 @@ using namespace sf;
 
 int width = 1024;
 int height = 768;
+int roadW = 2000;
+int segL = 200;    // segment length
+float camD = 0.84; // camera depth
+
+struct Line {
+    float x, y, z; // 3d center of line
+    float X, Y, W; // screen coord
+    float curve, spriteX, clip, scale;
+    Sprite sprite;
+
+    Line() {
+        spriteX = curve = x = y = z = 0;
+    }
+
+    void project(int camX, int camY, int camZ) {
+        scale = camD / (z - camZ);
+        X = (1 + scale * (x - camX)) * width / 2;
+        Y = (1 - scale * (y - camY)) * height / 2;
+        W = scale * roadW * width / 2;
+    }
+};
 
 void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2) {
     ConvexShape shape(4);
@@ -19,6 +40,15 @@ void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, 
 
 int main() {
     RenderWindow window(VideoMode(width, height), "GameDevLog");
+    std::vector<Line> lines;
+
+    for (int i = 0; i < 1600; i++) {
+        Line line;
+        line.z = i * segL;
+        lines.push_back(line);
+    }
+
+    int N = lines.size();
 
     while (window.isOpen()) {
         Event e;
@@ -30,7 +60,22 @@ int main() {
         }
 
         window.clear();
-        drawQuad(window, Color::Green, 500, 500, 200, 500, 300, 100);
+        // draw road
+        for (int n = 0; n < 300; n++) {
+            Line &l = lines[n % N];
+            l.project(0, 1500, 0);
+
+            Color grass = (n / 3) % 2 ? Color(16, 200, 16) : Color(0, 154, 0);
+            Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(0, 0, 0);
+            Color road = (n / 3) % 2 ? Color(107, 107, 107) : Color(105, 105, 105);
+
+            Line p = lines[(n - 1) % N]; // previous line
+
+            drawQuad(window, grass, 0, p.Y, width, 0, l.Y, width);
+            drawQuad(window, rumble, p.X, p.Y, p.W * 1.2, l.X, l.Y, l.W * 1.2);
+            drawQuad(window, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
+        }
+        
         window.display();
     }
 
