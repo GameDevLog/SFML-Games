e91dae5ffaa6e6c87c62ad639043554b97ada215
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-23 15:25:35 +0800

    feat(11 netwalk): pipe

--- src/11_NetWalk_Pipe_Puzzle/main.cpp
+++ src/11_NetWalk_Pipe_Puzzle/main.cpp
@@ -3,10 +3,33 @@
 
 using namespace sf;
 
+const int N = 6;
+int ts = 54; // tile size
+Vector2f offset(65, 55);
+
+struct pipe {
+    int orientation;
+    bool on;
+};
+
+pipe grid[N][N];
+
 int main() {
-    const int w = 320;
-    const int h = 480;
+    srand(time(0));
+
+    const int w = 390;
+    const int h = 390;
     RenderWindow window(VideoMode(w, h), "GameDevLog");
+
+    Texture t1, t2, t3, t4;
+    t1.loadFromFile("images/background.png");
+    t2.loadFromFile("images/comp.png");
+    t3.loadFromFile("images/server.png");
+    t4.loadFromFile("images/pipes.png");
+    t4.setSmooth(true);
+
+    Sprite sBackground(t1), sComp(t2), sServer(t3), sPipe(t4);
+    sPipe.setOrigin(27, 27);
 
     while (window.isOpen()) {
         Event e;
@@ -14,6 +37,19 @@ int main() {
         while (window.pollEvent(e)) {
             if (e.type == Event::Closed) {
                 window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(sBackground);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                pipe &p = grid[j][i];
+                sPipe.setTextureRect(IntRect(ts * 2, 0, ts, ts));
+                sPipe.setPosition(j * ts, i * ts);
+                sPipe.move(offset);
+                window.draw(sPipe);
             }
         }
 
